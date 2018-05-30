42931153b81d202ab0dfec5ca566c35846d5a558
X-wenhao <c.x.wenhao@gmail.com>
2018-05-30 19:12:37 +0800

    change validate

--- ball_in_box/validate.py
+++ ball_in_box/validate.py
@@ -5,7 +5,6 @@
 """
 
 import math
-E=1e-8
 def validate(circles, blockers):
     # Is circle in the box?
     for circle in circles:
@@ -14,10 +13,10 @@ def validate(circles, blockers):
         ymr = circle[1] - circle[2]
         ypr = circle[1] + circle[2]
 
-        if (not (xmr <= 1+E and xmr >=-1-E )) \
-           or (not (xpr <= 1+E and xpr >=-1-E )) \
-           or (not (ymr <= 1+E and ymr >=-1-E )) \
-           or (not (ypr <= 1+E and ypr >=-1-E )):
+        if (not (xmr <= 1 and xmr >=-1 )) \
+           or (not (xpr <= 1 and xpr >=-1 )) \
+           or (not (ymr <= 1 and ymr >=-1 )) \
+           or (not (ypr <= 1 and ypr >=-1 )):
             #print("0",circle)
             return False
 
@@ -30,7 +29,7 @@ def validate(circles, blockers):
                 r = circle[2]
                 bx = block[0]
                 by = block[1]
-                if math.sqrt((x - bx)**2 + (y - by)**2) -r< -E:
+                if math.sqrt((x - bx)**2 + (y - by)**2) < r:
                     #print("1",circle)
                     return False
 
@@ -45,7 +44,7 @@ def validate(circles, blockers):
             x2 = circle2[0]
             y2 = circle2[1]
             r2 = circle2[2]
-            if math.sqrt((x1 - x2)**2 + (y1 - y2)**2) -(r1 + r2)<-E:
+            if math.sqrt((x1 - x2)**2 + (y1 - y2)**2) <(r1 + r2):
                 #print("math:",math.sqrt((x1 - x2)**2 + (y1 - y2)**2),'  ',r1 + r2)
                 #print("2",circle)
                 return False
