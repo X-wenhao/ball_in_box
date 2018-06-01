aa6d9513dc5ac95845f6ea441ca752fd52f8f317
RookieBrain <39577545+494727889@users.noreply.github.com>
2018-06-01 21:13:28 +0800

    Update Hermite.py

--- ball_in_box/Test/RookieBrain/Hermite.py
+++ ball_in_box/Test/RookieBrain/Hermite.py
@@ -3,3 +3,14 @@ from scipy.optimize import fsolve
 from copy import copy
 from pdb import set_trace
 INFT = float(10**10)
+class Bound(object):
+  
+    def __init__(self,x,y,r):
+        self.x , self.y , self.r = x , y , r
+        
+    def fit(self,another_bound):
+      
+        if another_bound.x == INFT :
+            return self.x + self.r <= 1.0
+          
+        elif another_bound.x == - INFT :
